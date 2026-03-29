2d7a5ebd431f39f9ac4ff7ceaa753c4d9661d6c2
Krzysztof Muszyński <kontakt@codefia.pl>
2026-03-29 10:55:43 +0200

    TextInput

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,29 @@
 import { useState } from "react";
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
-    const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Counter: {count}</Text>
-
-            <Pressable style={styles.button} onPress={() => setCount(count + 1)}>
-                <Text style={styles.buttonText}>+1</Text>
-            </Pressable>
-
-            <Pressable style={[styles.button, {backgroundColor: '#E94560'}]} onPress={() => setCount(0)}>
-                <Text style={styles.buttonText}>Reset</Text>
-            </Pressable>
+            <Text style={styles.label}>Jak masz na imię?</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Wpisz swoje imie..."
+                value={name}
+                onChangeText={setName}
+            />
+            <Text style={styles.greeting} >
+                {name ? `Hello, ${name}!` : 'Wpisz swoje imie powyżej!'}
+            </Text>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#f0f4f8'},
-    title: {fontSize: 48, fontWeight: 'bold', marginBottom: 20},
-    button: {backgroundColor: '#61DAFB', paddingHorizontal: 32, paddingVertical: 12, borderRadius: 8, marginTop: 10},
-    buttonText: {fontSize: 18, fontWeight: 'bold', color: '#OA1628'},
+    container: {flex:1, justifyContent: 'center', padding: 24, backgroundColor: '#f0f4f8'},
+    label: {fontSize: 20, fontWeight: 'bold', marginBottom: 8},
+    input: {borderWidth: 1, borderColor: '#CED4DA', borderRadius: 8, padding: 12, fontSize: 16, backgroundColor: '#FFF'},
+    greeting: {backgroundColor: '#61DAFB', paddingHorizontal: 32, paddingVertical: 12, borderRadius: 8, marginTop: 10},
+    buttonText: {fontSize: 24, marginTop: 16, color: '#61DAFB', fontWeight: 'bold'},
 });
